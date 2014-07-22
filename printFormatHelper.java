f39d174db8a11824c276e15968e43f08cdc3e9f2
sujalmandal <ss6sujal@gmail.com>
2014-07-22 23:52:21 +0530

    the interface which must be implemented
    While writing a new format, must implement this and add logic to the overriden method.

---
+++ printFormatHelper.java
@@ -0,0 +1,8 @@
+package formatHelper;
+
+import alien.details.Details;
+
+public interface printFormatHelper			//implement this class
+{
+public abstract void printToFormat(Details detail);	  //override this method and write your own logic
+}
