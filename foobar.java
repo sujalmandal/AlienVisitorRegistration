15f5945b5068d75879e47d9f1ad16ebdf69446d0
sujalmandal <ss6sujal@gmail.com>
2014-07-23 00:10:24 +0530

    foobar.java
    Add your own logic here, or remove this class to see the effect !

---
+++ foobar.java
@@ -0,0 +1,13 @@
+package formatsToPrint;
+*Dummy format*
+
+public class foobar implements printFormatHelper
+{
+
+@Override
+	public void printToFormat(Details detail)// add your logic here or this will not work
+	{
+	
+	}
+
+}
