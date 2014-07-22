2737f8240b03eb11636138eb451182dabdc9699d
sujalmandal <ss6sujal@gmail.com>
2014-07-22 23:53:35 +0530

    the pdf plugin
    you can remove or add this class, no effect on main application.

--- pdf.java
+++ pdf.java
@@ -1,4 +1,4 @@
-package writer;
+package formatsToPrint;
 
 import java.io.FileOutputStream;
 
@@ -14,26 +14,28 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import formatHelper.*;
 
 /**********************************This class uses a API to write PDF files********************************
 *********************************visit http://sourceforge.net/projects/itext/*****************************/
-public class WritePdf 
+public class pdf implements printFormatHelper
 {
-  private Document document;
-  private PdfPCell cell; 				//Global declarations here
-  private String file =null; 
-  private Font bigFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,Font.BOLD);	//bigFont defined here
-
-  public void writeToPdf(Details alienDetail) // the main method which prints the data to the PDF file
+  private static Document document;
+  private static PdfPCell cell; 				//Global declarations here
+  private static String file =null; 
+  private static Font bigFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,Font.BOLD);	//bigFont defined here
+ 
+  
+  public void printToFormat(Details detail)
   {
 	  try
 	    {
-		  file="c:/"+alienDetail.getCodeName()+".pdf";
+		  file="c:/"+detail.getCodeName()+".pdf";
 	      document = new Document();	//create a new document object
 	      PdfWriter.getInstance(document, new FileOutputStream(file));
 	      document.open();		        //open the document
 	      addMetaData(document);        // add all metadata to the pdf file.
-	      addContents(document, alienDetail);// add the registration page
+	      addContents(document, detail);// add the registration page
 	      document.close();	         	//close the document after writing
 	      System.out.println("The details have been printed successfully :-) !");
 	      System.out.println("\nFind the file in your \"C:/\" drive, Thank you!");	//tell the location of the file
@@ -48,7 +50,7 @@ public class WritePdf
   
   /*************************helper methods below************************************/
   
-  private void addMetaData(Document document)   //Add metadata to the pdf file to be written
+  private static void addMetaData(Document document)   //Add metadata to the pdf file to be written
   {
     document.addTitle("Alien Registration");
     document.addSubject("Multunus Test");
@@ -56,7 +58,7 @@ public class WritePdf
     document.addAuthor("Sujal Mandal");
   }
 
-  private void addContents(Document document,Details alienDetail) throws DocumentException 
+  private static void addContents(Document document,Details alienDetail) throws DocumentException 
   {
     Paragraph myParagraph = new Paragraph(); 	//create a paragraph object
     addEmptyLine(myParagraph, 1); 				//add one empty line
@@ -66,7 +68,7 @@ public class WritePdf
     createTable(document,alienDetail);			    //create the detail tables
   }
 
-  private void createTable(Document doc,Details alienDetail) throws BadElementException 
+  private static void createTable(Document doc,Details alienDetail) throws BadElementException 
   {
     PdfPTable table = new PdfPTable(5); 	//create a new table object with 5 fields 
     
@@ -95,15 +97,14 @@ public class WritePdf
 	}
   }
 
-  private void addTableCell(PdfPTable table,String content )  ///method to write cells to the table
+  private static void addTableCell(PdfPTable table,String content )  ///method to write cells to the table
   {
 	  cell = new PdfPCell(new Phrase(content));					//cell object
 	  cell.setHorizontalAlignment(Element.ALIGN_CENTER);		//alignment options
 	  table.addCell(cell);										//add the cell to the table
   }
   
-  
-  private void addEmptyLine(Paragraph paragraph, int number) 
+  private static void addEmptyLine(Paragraph paragraph, int number) 
   {
     for (int i = 0; i < number; i++) 
     {
